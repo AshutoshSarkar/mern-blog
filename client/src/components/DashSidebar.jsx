b0de73be2e5d29caa02395a6406197389dcc761b
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-01-28 15:00:25 +0530

    completed image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiUser } from "react-icons/hi";
+
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState("");
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -13,16 +14,17 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"user"} labelColor="dark">
-            Profile
-          </Sidebar.Item>
+            <Sidebar.Item active={tab === 'profile'} icon={HiUser} as='div' label={"user"} labelColor="dark" >
+              Profile
+            </Sidebar.Item>
           </Link>
-          <Sidebar.Item  icon={HiArrowSmRight} classname="cursor-pointer">
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
