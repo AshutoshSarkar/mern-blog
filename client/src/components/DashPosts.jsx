66fea68ebe841750dbf647a4fec24717a7eab779
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-02-01 17:33:09 +0530

    added show more functionality into post on dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -2,10 +2,11 @@ import { Table } from "flowbite-react";
 import React from "react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function DashPosts() {
   const [userPosts, setUserPosts] = useState([]);
+  const[showMore, setShowMore] = useState(true);
   console.log(userPosts);
   const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
@@ -15,6 +16,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,25 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+    const handleShowMore = async () => {
+        const startIndex= userPosts.length;
+       try {
+        const res= await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+        const data = await res.json();
+        if(res.ok){
+            setUserPosts([...userPosts, ...data.posts]);
+            if(data.posts.length < 9){
+                setShowMore(false);
+              }
+        }
+        
+       } catch (error) {
+        console.log(error.message);
+        
+       }
+
+    }
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -46,35 +69,51 @@ export default function DashPosts() {
               </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              <Table.Body  className="divide-y ">
+              <Table.Body className="divide-y ">
                 <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
                   </Table.Cell>
                   <Table.Cell>
                     <Link to={`/post/${post.slug}`}>
-                        <img src={post.image} alt={post.title} className="w-20 h-10 object cover bg-gray-500"/>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object cover bg-gray-500"
+                      />
                     </Link>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className="font-medium text-gray-900 dark:text-white " to={`/post/${post.slug}`}>{post.title}</Link>
-                    
+                    <Link
+                      className="font-medium text-gray-900 dark:text-white "
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span className="font-medium text-red-500 hover:underline cursor-pointer">Delete</span>
+                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                      Delete
+                    </span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className='text-teal-500'  to={`/update-post/${post._id}`}>
-                    <span>Edit</span>
+                    <Link
+                      className="text-teal-500"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
                     </Link>
-                    
-                    
                   </Table.Cell>
                 </Table.Row>
               </Table.Body>
             ))}
           </Table>
+          {
+           showMore && (<button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7" >
+            Show more</button>
+           )
+          }
         </>
       ) : (
         <p>You have no posts yet !</p>
