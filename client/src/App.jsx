c748ca44fdd8c6e1b3ba7628941545b0458bbd3e
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-02-05 18:50:08 +0530

    added addtoaction.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 export default function app() {
   return (
@@ -31,6 +32,7 @@ export default function app() {
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage/>}/>
       </Routes>
       <Footer />
     </BrowserRouter>
