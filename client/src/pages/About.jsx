05705c5f22fd7a872edb8f51efbfeac0238d9793
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-02-08 14:35:21 +0530

    Completed dashboard functionality to the application

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,12 +1,35 @@
-
-import React from 'react'
-
 export default function About() {
   return (
-    <div className='min-h-screen'>
-    <h1>
-      About
-    </h1>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-300'>
+      <div className='max-w-2xl mx-auto p-6 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>
+            About Ashutosh Sarkar's Blog
+          </h1>
+          <div className='text-lg text-gray-700 dark:text-gray-400 flex flex-col gap-6'>
+            <p>
+              Welcome to Ashutosh Sarkar's Blog! This blog was created by Ashutosh Sarkar
+              as a personal project to share his thoughts and ideas with the
+              world. Ashutosh is a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Ashutosh is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -5,15 +5,13 @@ import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
 import PostCard from '../components/PostCard';
 
-
-
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
   const [recentPosts, setRecentPosts] = useState(null);
-  
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -41,7 +39,7 @@ export default function PostPage() {
   useEffect(() => {
     try {
       const fetchRecentPosts = async () => {
-        const res = await fetch(`/api/post/getposts?limit=4`);
+        const res = await fetch(`/api/post/getposts?limit=3`);
         const data = await res.json();
         if (res.ok) {
           setRecentPosts(data.posts);
@@ -90,21 +88,15 @@ export default function PostPage() {
       <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      
-    <CommentSection postId={post._id}/>
-    <div className="flex flex-col justify-center items-center mb-5">
-      <h1 className='text-xl mt-5'>Recent Article</h1>
-      <div className="flex flex-wrap gap-5 mt-5 justify-center">
-        {
-          recentPosts && recentPosts.map((post, index) => (
-             
-            <PostCard key={post._id} post={post}/>
-          ))
-        }
+      <CommentSection postId={post._id} />
 
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
       </div>
-      </div>
-
     </main>
   );
 }
